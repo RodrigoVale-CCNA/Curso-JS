e01e63e72f194c8107b0ba33dd738abb6c12b0c7
Rodrigo Vale <irsv98@hotmail.com>
2025-01-28 01:18:31 -0300

    update
    3 paginas

--- Curso JS/CursoJS/Modulo D/PagVolei/scriptVolei.js
+++ Curso JS/CursoJS/Modulo D/PagVolei/scriptVolei.js
@@ -3,21 +3,20 @@ function verificar () {
     var ano = data.getFullYear()
     var fAno = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    /*var fnome = document.getElementById('txtnome')   
+    var fnome = document.getElementById('txtnome')   
     var fpos = document.getElementsByName('radpos')
+    var nome = String(fnome.value)
     var posicao = ''
-    if(fpos[0].checked){
-        var posicao ='Levantador'
-    }else if(fpos[1].checked){     
-        var posicao ='Ponteiro'
-    }else if(fpos[2].checked){
-        var posicao ='Líbero'
-    }else{
-        window.alert(`[ERROR] Posição inválida`)
-    }*/
-    if(fAno.value.length == 0 || fAno.value > ano){
+    if(fAno.value.  length == 0 || fAno.value > ano){
         window.alert(`[ERRO]Verifique os dados e tente novamente`)
     }else{
+        if(fpos[0].checked){
+            var posicao = 'Levantador'
+        }else if(fpos[1].checked){
+            var posicao = 'Ponteiro'
+        }else{
+            var posicao = 'Líbero'
+        }
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fAno.value)
         var genero = ''
@@ -26,7 +25,8 @@ function verificar () {
         }else if (fsex[1].checked){
             genero = 'Mulher'
         }
-        res.innerHTML =`Detectamos: ${genero} com ${idade} anos`
-        res.innerHTML =`${fnome} você joga na posição: ${posicao}`
+        res.innerHTML =`Detectamos: ${genero} com ${idade} anos <br> 
+        <strong>${nome}</strong> você joga como <strong>${posicao}</strong>`
+        
     }
 }
